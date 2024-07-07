f24cae104872ec0920b5c9dbadf703415e039c5b
alessioarcara <Spammer123@>
2024-07-07 22:53:04 +0200

    Fixed bash script runner

--- runner.sh
+++ runner.sh
@@ -3,14 +3,13 @@
 METHOD=$1
 MODEL=$2
 SOLVER=$3
+LOWER_BOUND=$4
+UPPER_BOUND=$5
 SCRIPT="mcp.py"
 
-for i in $(seq -w 1 10); do 
+for i in $(seq -w $LOWER_BOUND $UPPER_BOUND); do 
     INSTANCE="./Instances/inst$i.dat"
-    python $SCRIPT $INSTANCE $METHOD $MODEL $SOLVER 275
-done
 
-for i in 13 16 19; do 
-    INSTANCE="./Instances/inst$i.dat"
     python $SCRIPT $INSTANCE $METHOD $MODEL $SOLVER 275
+
 done
